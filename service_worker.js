9c510d0f5281005b3ba49fbcb7c03e368ae46c32
Lisa Uloh <lissyuloh8@gmail.com>
2023-06-15 13:08:08 +0400

    edit service worker

--- service_worker.js
+++ service_worker.js
@@ -21,7 +21,7 @@ self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching app shell');
+            console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
         })
     );
